053bb0437ad50255dd202c2bd685ee24865cfee3
dangertater <106702944+dangertater@users.noreply.github.com>
2022-11-06 13:04:30 -0700

    pre eric fuckery

--- first-react-project/src/App.js
+++ first-react-project/src/App.js
@@ -1,5 +1,25 @@
+//https://www.youtube.com/watch?v=hQAHSlTtcmY&t=310s
+//----react project walkthrough abouve
+//https://www.youtube.com/watch?v=O6P86uwfdR0
+//----react hooks explination above
+import React, { useState } from "react"
+import TodoList from "./TodoList.js"
+
+//last thing i did was piece together string todo 1 and todo 2 on line 13 gets passed to TodoList.js and in turn Todo.js eventually populating the webpage with both strings
+//q4e, wtf is the '={todos}' on line 16?
+
 function App() {
-	return null
+	//todos below is every one of the todos in the useState, second variable is function we call to update the todos
+	const [todos, setTodos] = useState(["Todo 1", "Todo 2"])
+	return (
+		<>
+			<TodoList todos={todos} />
+			<input type="text" />
+			<button>add todo </button>
+			<button>clear complete</button>
+			<div>number left todo</div>
+		</>
+	)
 }
 
 export default App
